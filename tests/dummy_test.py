f7f3e2b73c7c66f21ab98504653bb4561d3abbe2
gc1523 <guyconnolly15@gmail.com>
2025-07-27 00:35:11 +0100

    chore: move CI to correct directory

---
+++ tests/dummy_test.py
@@ -0,0 +1,6 @@
+def test_dummy():
+    """
+    This is a dummy test function to ensure the test suite runs.
+    It should be replaced with actual tests.
+    """
+    assert True  # Placeholder assertion
